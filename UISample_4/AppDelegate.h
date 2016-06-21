3b84dbe23dd2b2a8262a3515f01a488b1cfb6194
Imran Naseem <imrann@vijayalakshmis-mac-mini.local>
2016-06-21 17:07:12 +0530

    Initial Commit

---
+++ UISample_4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISample_4
+//
+//  Created by Imran Naseem on 21/06/16.
+//  Copyright © 2016 Imran Naseem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISample_4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISample_4
+//
+//  Created by Imran Naseem on 21/06/16.
+//  Copyright © 2016 Imran Naseem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
